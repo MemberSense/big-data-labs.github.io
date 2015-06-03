9b301a6823660b1159e15dda759ddc7c4713bcb1
Dipak Yadav <deepakyadav11153@gmail.com>
2015-06-03 08:46:11 +0530

    building feed simplified version

---
+++ base/feed/js/feed.js
@@ -0,0 +1,17 @@
+var app = angular.module('feed', []);
+
+app.directive("gfSlickCarousel", function() {
+    return {
+        // Restrict it to be an attribute in this case
+        restrict: 'A',
+        // responsible for registering DOM listeners as well as updating the DOM
+        link: function(scope, element, attrs) {
+            $(element).slick();
+            console.log("slick initialized");
+        }
+    };
+});
+
+app.controller('FeePageController', function() {
+    
+});
