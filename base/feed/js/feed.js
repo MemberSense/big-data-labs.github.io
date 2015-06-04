a605f7b96397583139ae6c99c4e0b86fc4111171
Dipak Yadav <deepakyadav11153@gmail.com>
2015-06-04 09:03:40 +0530

    feed page

--- base/feed/js/feed.js
+++ base/feed/js/feed.js
@@ -1,17 +1,36 @@
-var app = angular.module('feed', []);
+var app = angular.module('feed', ['jsonFormatter']);
 
-app.directive("gfSlickCarousel", function() {
+app.directive("gfSlickCarousel", function($timeout) {
     return {
         // Restrict it to be an attribute in this case
         restrict: 'A',
         // responsible for registering DOM listeners as well as updating the DOM
         link: function(scope, element, attrs) {
-            $(element).slick();
-            console.log("slick initialized");
+            $timeout(function() {
+                 $(element).slick(scope.$eval(attrs.gfSlickCarousel));
+                 console.log("slick initialized");
+            });
+            
+            
         }
     };
 });
 
-app.controller('FeePageController', function() {
-    
+app.controller('FeePageController', function($scope, $http) {
+    $http.jsonp('http://georgiafacts.org/smart/api/itemfeed?dynamic=1&tid=16400&callback=JSON_CALLBACK&tid=16400').
+    success(function (data) {
+        $scope.data = data;
+        $scope.images = [];
+        console.log(data);
+        for(item in data.items) {
+            for(image in data.items[item].images) {
+                $scope.images.push(data.items[item].images[image].large);
+            }
+        }
+        console.log($scope.images);
+    }).
+    error(function (data) {
+        $scope.data = "Request failed";
+    });
 });
+
